51e16e3b248cdd0fa8a808394956a3686c0930db
Adam Pacanowski <pacibum@gmail.com>
2021-09-04 13:44:37 +0200

    Add configReader.getAllFileNames method test

--- src/configReader.test.ts
+++ src/configReader.test.ts
@@ -1,5 +1,32 @@
-describe('1', () => {
-  it('1.1', () => {
+import { expect } from '@jest/globals';
+import ConfigReader from './configReader';
 
+describe('configReader', () => {
+  it('tests getAllFileNames method', () => {
+    const dirPath = 'src/jestFixtures/configReader/';
+    const globPattern = `${ dirPath }**/*.spec.js`;
+    const expectedResult = [
+      `${ dirPath }1.spec.js`,
+      `${ dirPath }2.spec.js`,
+      `${ dirPath }someDir/4.spec.js`
+    ];
+    const notExpectedResult = [
+      `${ dirPath }/3.nospec.js`,
+    ];
+    
+    
+    const configReader = new ConfigReader(globPattern);
+    const fileNames = configReader.getAllFileNames();
+
+
+    expectedResult.forEach(fileName => {
+      expect(fileNames).toContain(fileName);
+    });
+
+    notExpectedResult.forEach(fileName => {
+      expect(fileNames).not.toContain(fileName);
+    });
+
+    expect(fileNames.length).toEqual(3);
   });
 });
